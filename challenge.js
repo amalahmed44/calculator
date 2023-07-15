0db248c3a016db528f186dc11565c07dae1ba8d6
amalali8 <amalza958@gmail.com>
2023-07-15 04:44:43 +0300

    answer

--- challenge.js
+++ challenge.js
@@ -1,2 +1,21 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+function calculator(num1 , num2 , operator){
+let result;
+if(operator==="+"){
+    result= num1 + num2
+}else if(operator==="-"){
+    result=num1-num2
+}else if(operator==="/"){
+    result=num1/num2
+}else if(operator==="*"){
+    result=num1*num2
+}
+
+console.log("The result is: " + result);
+}
+calculator(5, 11, "+"); 
+calculator(8, 9, "-");
+calculator(1, 1, "/");
+calculator(5, 5, "*"); 
+calculator(6, 9, "%"); 
